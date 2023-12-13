ec30591e1b413828955e99e16947bb6d5e515bd4
jp1405 <izairi.q@gmail.com>
2023-12-13 19:32:24 +0100

    Add files via upload

---
+++ db_a9d850_bdf_1.sql
@@ -0,0 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+
+--
+-- Database: `db_a9d850_bdf_1`
+--
+
+-- --------------------------------------------------------
+
+
+INSERT INTO `products` (`id`, `name`, `price`, `sku`, `type`, `size`, `weight`, `height`, `width`, `length`) VALUES
+(63, 'Table2', '500', 'Bookie133434', 'Book', NULL, '5', NULL, NULL, NULL),
+(67, 'Table2', '500', 'Bookie34', 'Furniture', NULL, NULL, '5000', '5000', '5000'),
+(68, 'NameTest002', '25', 'SKUTest002', 'Furniture', NULL, NULL, '200', '200', '200'),
+(69, 'NameTest000', '555', 'NameTest000', 'Electronics', '7', NULL, NULL, NULL, NULL),
+(70, 'NameTest001', '3', 'NameTest001', 'Book', NULL, '5', NULL, NULL, NULL),
+(71, 'Qëndrim Izairi', '444', 'dfbhadhf', 'Book', NULL, '4', NULL, NULL, NULL),
+(72, 'Roman', '77', 'kljklm\'\'\'', 'Furniture', NULL, NULL, '56', '6', '66'),
+(73, 'Dimi', '555', 'testipalidhje', 'Furniture', NULL, NULL, '4', '5', '4'),
+(74, 'Bohdana Stelmashchuk', '555', 'testipalidhje555s', 'Furniture', NULL, NULL, '4', '5', '4'),
+(75, 'NameTest001', '25', 'SKUTest001', 'Book', NULL, '200', NULL, NULL, NULL),
+(76, 'NameTest000', '25', 'SKUTest000', 'Electronics', '200', NULL, NULL, NULL, NULL),
+(79, 'jjsjd', '8', 'bookkssss', 'Furniture', NULL, NULL, '44', '55', '66'),
+(80, 'Qëndrim Izairi', '444', 'ssaaff555dddf', 'Electronics', '4', NULL, NULL, NULL, NULL),
+(81, 'Qëndrim Izairi', '3', 'testifinal', 'Electronics', '5', NULL, NULL, NULL, NULL),
+(82, 'Dimi', '3', 'testipalidhjefibnal', 'Furniture', NULL, NULL, '44', '5', '77'),
+(83, 'Qëndrim Izairi', '3', 'testipalidhje555ff', 'Furniture', NULL, NULL, '4', '3', '2'),
+(84, 'eerr', '4', 'Bookie34432', 'Electronics', '5', NULL, NULL, NULL, NULL);
+
